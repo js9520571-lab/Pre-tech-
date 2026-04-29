f140bb734b74bbabede22ad372e1110358407de6
js9520571-lab <js9520571@gmail.com>
2026-04-29 17:06:39 +0530

    Create App.js to import and render JasmeetDashboard with dark background

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import JasmeetDashboard from './JasmeetDashboard';
+
+const App = () => {
+    return (
+        <div style={{ backgroundColor: 'black', color: 'white', height: '100vh' }}>
+            <JasmeetDashboard />
+        </div>
+    );
+};
+
+export default App;
